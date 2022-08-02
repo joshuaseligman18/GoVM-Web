ed6c801b95db493336c9d1c4e2fa0c3342092791
Josh Seligman <joshua.seligman1@marist.edu>
2022-08-02 09:41:25 -0400

    Added the footer

---
+++ client/src/components/footer.jsx
@@ -0,0 +1,11 @@
+import footerStyles from './../styles/footer.module.scss'
+
+const Footer = () => {
+	return (
+		<footer id={footerStyles.footer}>
+			<h1>&copy; 2022 Joshua Seligman. All rights reserved.</h1>
+		</footer>
+	)
+}
+
+export default Footer
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,16 @@
 import ReactDOM from 'react-dom'
-import Navbar from './components/Navbar'
+import Navbar from './components/navbar'
+import Footer from './components/footer'
 
 
 const App = () => {
 	return (
 		<>
 			<Navbar />
+			<div>
+				Hello world
+			</div>
+			<Footer />
 		</>
 	)
 }
