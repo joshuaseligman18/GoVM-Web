9ff7a1191af2ae59bbbefd7d5cb0f24471457f35
Josh Seligman <joshua.seligman1@marist.edu>
2022-08-02 11:23:07 -0400

    Modularized some styling and fixed the font size issues

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import navStyles from './../styles/navbar.module.scss'
 // Component for the navbar
 const Navbar = () => {
 	return (
-		<nav id={navStyles.navbar}>
+		<nav id={navStyles.navbar} className={navStyles.bar}>
 			<h1>GoVM</h1>
 		</nav>
 	)
--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -3,7 +3,7 @@ import footerStyles from './../styles/footer.module.scss'
 // Component for the footer
 const Footer = () => {
 	return (
-		<footer id={footerStyles.footer}>
+		<footer id={footerStyles.footer} className={footerStyles.bar}>
 			<h1>&copy; 2022 Joshua Seligman. All rights reserved.</h1>
 		</footer>
 	)
