8b98cb72beebc059c1d3c84dba7c130f045feff3
Josh Seligman <joshua.seligman1@marist.edu>
2022-08-04 13:04:53 -0400

    Displaying details about each program in the queue now

--- client/src/components/queueStatusItem.jsx
+++ client/src/components/queueStatusItem.jsx
@@ -1,10 +1,25 @@
 import queueStyles from './../styles/queue.module.scss'
+import QueueSelectionContext from './queueSelectionContext'
+
 
 const QueueStatusItem = () => {
+
     return (
-        <div id={queueStyles.statusItem} className={queueStyles.queueSection}>
-            Hello world
-        </div>
+        <QueueSelectionContext.Consumer>
+            {({selectedProg, updateProg}) => (
+                (Object.keys(selectedProg).length > 0) ?
+                    <div id={queueStyles.statusItem} className={queueStyles.queueSection}>
+                        <p>Id: {selectedProg.id}</p>
+                        <p>Name: {selectedProg.progName}</p>
+                        <p>Date Created: {selectedProg.created.substring(0, 10)}</p>
+                        <p>Time Created: {selectedProg.created.substring(11, 19)}</p>
+                    </div>
+                :   <div id={queueStyles.statusItem} className={queueStyles.queueSection}>
+                        <p>Select a program in the queue to view more details.</p>
+                    </div>
+
+            )}
+        </QueueSelectionContext.Consumer>
     )
 }
 
