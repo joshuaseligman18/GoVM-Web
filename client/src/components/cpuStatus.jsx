87be796700f367db785ff96dee8524f00c76dd7d
Josh Seligman <joshua.seligman1@marist.edu>
2022-08-09 14:58:01 -0400

    Showing the basic registers now

--- client/src/components/cpuStatus.jsx
+++ client/src/components/cpuStatus.jsx
@@ -1,13 +1,40 @@
 import cpuStyles from './../styles/cpu.module.scss'
 
+import { hexString } from './../util/util'
+
 const CpuStatus = (props) => {
 
-    const cpuStatus = props.cpuStatus
+    const cpuStatus = props.cpuStatus !== undefined ? props.cpuStatus.cpu : null
+
+    const statusComponents = []
+
+    for (let i = 0; i < 32; i++) {
+        let text = ''
+        if (i === 0) {
+            text += 'PC: '
+        } else {
+            text += `X${i - 1}: `
+        }
+
+        if (cpuStatus === null) {
+            text += hexString(0, 16)
+        } else {
+            if (i === 0) {
+                text += hexString(cpuStatus.programCounter, 16)
+            } else {
+                text += hexString(cpuStatus.registers[i - 1], 16)
+            }
+        }
+
+        statusComponents.push(<p>{text}</p>)
+    }
 
     return (
         <div id={cpuStyles.cpuStatusArea}>
             <h2 id={cpuStyles.title}>CPU Status</h2>
-            <p>{JSON.stringify(cpuStatus)}</p>
+            <div id={cpuStyles.statusTable}>
+                {[...statusComponents]}
+            </div>
         </div>
     )
 }
--- client/src/components/queueArea.jsx
+++ client/src/components/queueArea.jsx
@@ -15,6 +15,7 @@ const QueueArea = (props) => {
     return (
         <div id={queueStyles.queueArea}>
             <h3 id={queueStyles.queueAreaTitle}>Program Queue</h3>
+            <p id={queueStyles.note}>Note: It takes a few seconds for the queue to fully update</p>
             <div id={queueStyles.queues}>
                 <QueueContext.Provider value={{selectedProg: selected, updateProg: (newProg) => setSelected(newProg), queue: queue}}>
                     <div id={queueStyles.queueList} className={queueStyles.queueSection}>
